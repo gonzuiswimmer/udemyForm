427d7de1752662d668215b869f7bc9e18615d473
gonzuiswimmer <gonzuiswimmer@gmail.com>
2023-09-07 13:53:16 +0900

    configディレクトリの修正

--- routes.rb
+++ routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :orders, only: [:new, :create] do
+    collection do
+      post :confirm
+      get :complete
+    end
+  end
 end
